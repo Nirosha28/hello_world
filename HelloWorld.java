20a6531b4670164b2c0540976a289039f04554ae
Nirosha <k.nirosha428@gmail.com>
2022-03-17 10:59:54 +0000

    merging

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
-<<<<<<< HEAD
-    System.out.println("Hello,good morning World");
-    System.out.println("Hello,good morning World");
-  }
-=======
     System.out.println("hi");
     System.out.println("Hello,happy holi");
   System.out.println("Hello,happy holi");
 }
->>>>>>> 6b6141ee226d5d703b129fbc28a085c246e7f9be
 }
