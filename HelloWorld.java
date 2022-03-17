48f5a09fc43920900761f17f3361486120289e64
Nirosha <k.nirosha428@gmail.com>
2022-03-17 10:37:45 +0000

    poll scm checking

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello,good morning World");
     System.out.println("Hello,good morning World");
-    System.out.println("Hello,good morning World");
   }
 }
